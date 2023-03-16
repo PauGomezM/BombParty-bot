3f15c3565ce5a2ca5e141f9e9e378dbb00edbccf
Aleix-PC <aleix.fort45@gmail.com>
2023-03-16 03:15:47 +0100

    Updated

--- main.py
+++ main.py
@@ -4,38 +4,70 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 used_words = []
 
 driver_service = Service('D:\Code\Drivers\chromedriver.exe')
 game_driver = webdriver.Chrome(service=driver_service)
 dict_driver = webdriver.Chrome(service=driver_service)
 
+
 # lobby = input('Lobby code? ')
 # driver.get(f'https://jklm.fun/{lobby}')
-game_driver.get('https://jklm.fun/BBJM')  # Hardcoded lobby (for testing)
-dict_driver.get('https://www.listapalabras.com/palabras-con.php')
-time.sleep(2)
+# player_username = input('Username? ')
 
+dict_driver.get('https://www.listapalabras.com/palabras-con.php')
+
+player_username = 'hermano'  # Hardcoded username (for testing)
+game_driver.get('https://jklm.fun/BBSG')  # Hardcoded lobby (for testing)
+
+inputElement = game_driver.find_elements(By.TAG_NAME, 'input')[1]
+for _ in range(11):
+    inputElement.send_keys(Keys.BACKSPACE)
+inputElement.send_keys(player_username)
+inputElement.send_keys(Keys.ENTER)
+
+time.sleep(3)
 # TODO: Must wait for user to join the game to proceed.
 
 # The game screen itself is an iframe. As selenium cannot access iframe data directly, we must first switch to the frame
 iframe = game_driver.find_element(By.TAG_NAME, 'iframe')  # Find iframe
 game_driver.switch_to.frame(iframe)  # Switch to iframe
-time.sleep(3)
+time.sleep(1)
 
 game_on = True
 while game_on:
+
+    turn = False
+    while not turn:
+        time.sleep(0.05)
+        turn = game_driver.find_element(By.CLASS_NAME, 'selfTurn').is_displayed()
+        if turn:
+            turn = True
+
     syllable = game_driver.find_element(By.CLASS_NAME, 'syllable').text  # Fetch syllable from its respective div
+
     inputElement = dict_driver.find_element(By.ID, 'busqueda')
     inputElement.send_keys(syllable)
     inputElement.send_keys(Keys.ENTER)
 
-    word_found = False
+    # search = True
+    # i = 0
+    # while search:
+    #     word = dict_driver.find_elements(By.ID, 'palabra_resultado')[i]
+    #     if word.text not in used_words:
+    #         used_words.append(word.text)
+    #         search = False
+    #     i += 1
 
     word = dict_driver.find_element(By.ID, 'palabra_resultado')
-    print(word.text)
-    print(used_words)
-    game_on = False
 
-game_driver.quit()
-dict_driver.quit()
+    narrowedElement = game_driver.find_element(By.CLASS_NAME, 'selfTurn')
+    inputElement = narrowedElement.find_element(By.TAG_NAME, 'input')
+    inputElement.send_keys(word.text)
+    inputElement.send_keys(Keys.ENTER)
+
+
+
+
+
--- test.py
+++ test.py
@@ -7,19 +7,17 @@ import time
 used_words = []
 
 
+player_username = 'stoupeaks'
+
 driver_service = Service('D:\Code\Drivers\chromedriver.exe')
-dict_driver = webdriver.Chrome(service=driver_service)
-dict_driver.get('https://www.listapalabras.com/palabras-con.php')
-time.sleep(2)
+driver = webdriver.Chrome(service=driver_service)
+driver.get('https://jklm.fun/BBJM')
 
+time.sleep(603)
 
-inputElement = dict_driver.find_element(By.ID, 'busqueda')
-inputElement.send_keys('lol')
+inputElement = driver.find_elements(By.TAG_NAME, 'input')[1]
+inputElement.send_keys(player_username)
 inputElement.send_keys(Keys.ENTER)
 
-
-for i in range(10):
-    word = dict_driver.find_elements(By.ID, 'palabra_resultado')
-    print(word[i].text)
 
 
