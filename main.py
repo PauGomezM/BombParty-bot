db8e8afb09784fceb9bb03f9aee016c513891b21
Aleix-PC <aleix.fort45@gmail.com>
2023-03-17 03:29:46 +0100

    Added features

--- main.py
+++ main.py
@@ -5,30 +5,42 @@ from selenium.webdriver.common.keys import Keys
 import time
 import random
 
-lobby = 'YYYC'
-
 words_list = []
 used_words = []
-common_misspelled_letters = 'acourhmneio'
+languages = ['en', 'es']
+common_misspelled_letters = 'abcdefgiorml'
+
+# Inputs
+while True:
+    player_username = input('Username? ')
+    if 2 <= len(player_username) <= 14:
+        break
+while True:
+    lobby = input('Lobby code? ').upper()
+    if len(lobby) == 4 and lobby.isalpha():
+        break
+while True:
+    language = input('Language? ... en/es? ').lower()
+    if language in languages:
+        break
+
+# player_username = 'hermano'  # Hardcoded username (for testing)
 
 # Save words in a list
-with open(f'./es_words.txt') as file:
+with open(f'./word_list/{language}.txt') as file:
     words = file.readlines()
 
 # Iterate each word to replace and remove unwanted characters
 for word in words:
     word = word.replace('Ã±', 'n')
+    word = word.replace('ñ', 'n')
     words_list.append(word.strip())
-
-# lobby = input('Lobby code? ')
-# player_username = input('Username? ')
-player_username = 'hermano'  # Hardcoded username (for testing)
 
 driver_service = Service('./chromedriver.exe')
 game_driver = webdriver.Chrome(service=driver_service)
 
-# driver.get(f'https://jklm.fun/{lobby}')
-game_driver.get(f'https://jklm.fun/{lobby}')  # Hardcoded lobby (for testing)
+game_driver.get(f'https://jklm.fun/{lobby}')
+# game_driver.get('https://jklm.fun/SEUJ')  # Hardcoded lobby (for testing)
 
 # Enters username
 inputElement = game_driver.find_elements(By.TAG_NAME, 'input')[1]
@@ -41,26 +53,23 @@ time.sleep(2)
 # The game screen itself is an iframe. As selenium cannot access iframe data directly, we must first switch to the frame
 iframe = game_driver.find_element(By.TAG_NAME, 'iframe')  # Find iframe
 game_driver.switch_to.frame(iframe)  # Switch to iframe
-time.sleep(4)
+time.sleep(3)
 
 # Main game loop
 game_on = True
 while game_on:
 
     # Waits for turn
-    turn = False
-    while not turn:
+    while True:
         time.sleep(0.2)
         own_turn = game_driver.find_element(By.CLASS_NAME, 'selfTurn').is_displayed()
         if own_turn:
-            turn = True
+            break
 
     # Fetch syllable
-    syl = game_driver.find_element(By.CLASS_NAME, 'syllable').text
-    syl = syl.lower()
-
+    syl = game_driver.find_element(By.CLASS_NAME, 'syllable').text.lower()
     # Set a random words list starting point
-    x = random.randint(0, 610000)
+    x = random.randint(0, len(words_list) - len(words_list)//4)
     # For each word in the list, check if it contains the desired syllable. Not accepted if already used before.
     for i in range(x, len(words_list)):
         if syl in words_list[i] and words_list[i] not in used_words:
@@ -76,10 +85,8 @@ while game_on:
     time.sleep(think_time)
     # Type each letter
     chance = random.randint(1, 100)
-    repeat_flag = False
 
     for i in range(len(word)):
-
         # Misspell case 1 (WIP)
         # if chance == 1:
         #     letter_pos = random.randint(0, len(word) - 1)
@@ -105,9 +112,9 @@ while game_on:
         time.sleep(type_time)
 
         # Adds chance to make mistake and backspace
-        chance = random.randint(1, 45)
+        chance = random.randint(1, 70)
         if chance == 2:
-            n_misspells = random.randint(1, 3)
+            n_misspells = random.randint(1, 2)
             # misspells n amount of letters
             for _ in range(n_misspells):
                 letter = random.randint(0, len(common_misspelled_letters) - 1)
@@ -122,4 +129,4 @@ while game_on:
     if game_driver.find_element(By.CLASS_NAME, 'selfTurn').is_displayed():
         inputElement.send_keys(Keys.ENTER)
 
-    time.sleep(1)
+    time.sleep(0.5)
